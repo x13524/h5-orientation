db9eaa1cefd78b574a594cc1afc290d69d4b2c02
xucanhui <xucanhui1@kingsoft.com>
2021-07-07 15:46:32 +0800

    feat: 为了解决部分特殊浏览器(vivo x9与华为p9)在软键盘被唤醒时的横竖屏的问题; 优先使用orientationchange 事件进行判断, 如浏览器不支持该事件再使用 onResize 事件使用宽高判断, 尽量减少误判

--- src/index.js
+++ src/index.js
@@ -3,47 +3,78 @@ import cx from 'classnames'
 import styles from './index.less'
 import {oneOf, string, func} from 'prop-types'
 
+const ORIENTATION_TEXT = {
+  横屏: 'landscape',
+  竖屏: 'vertical'
+}
+
 export default class Orientation extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentOrientation: null
+      currentOrientation: null,
+    }
+    this.isOrientationChangeSupported = window.screen && window.screen.orientation && window.screen.orientation.angle !== undefined && 'onorientationchange' in window
+  }
+
+  detectResize() {
+    if (this.isOrientationChangeSupported) {
+      // 如果当前浏览器支持使用 onorientationchange , 则使用该方法进行横竖屏判断
+      this.onOrientationchange()
+      window.addEventListener('orientationchange', this.onOrientationchange, false)
+      this.dispose = () => window.removeEventListener('orientationchange', this.onOrientationchange, false)
+    } else {
+      // 如果不支持, 则通过 resize 方法判断横竖屏
+      this.onPageResize()
+      window.addEventListener('resize', this.onPageResize, false)
+      this.dispose = () => window.removeEventListener('resize', this.onPageResize, false)
     }
   }
 
+  // 使用手机自带的横竖屏判断
+  onOrientationchange = () => {
+    const angle = window.screen.orientation.angle
+    if (angle === 0 || angle === 180 /* 竖屏 */) {
+      this.onChange(ORIENTATION_TEXT['竖屏'])
+    } else {
+      this.onChange(ORIENTATION_TEXT['横屏'])
+    }
+  }
+
+  onChange = type => {
+    this.setState({currentOrientation: type})
+  }
+
   componentDidMount() {
-    this.onPageResize()
-    window.addEventListener('resize', this.onPageResize, false)
+    this.detectResize()
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.onPageResize, false)
+    this.dispose()
   }
 
   onPageResize = () => {
     const {currentOrientation} = this.state
     const timer = setTimeout(() => {
-      if (window.innerWidth >= window.innerHeight && currentOrientation !== 'landscape') {
-        this.setState({
-          currentOrientation: 'landscape'
-        })
-      }
-      if (window.innerWidth < window.innerHeight && currentOrientation !== 'vertical') {
-        this.setState({
-          currentOrientation: 'vertical'
-        })
+      if (window.innerWidth >= window.innerHeight && currentOrientation !== ORIENTATION_TEXT['横屏']) {
+        this.onChange(ORIENTATION_TEXT['横屏'])
+      } else if (window.innerWidth < window.innerHeight && currentOrientation !== ORIENTATION_TEXT['竖屏']) {
+        this.onChange(ORIENTATION_TEXT['竖屏'])
       }
       clearTimeout(timer)
     }, 66)
   }
 
   renderContent() {
-    const {promptImg, renderContent} = this.props
+    const {promptImg, renderContent, orientation} = this.props
     if (renderContent && typeof renderContent === 'function') {
       return renderContent()
     }
     return (
-      <img className={styles.promptImg} src={promptImg} />
+      <div>
+        <img className={styles.promptImg} src={promptImg} alt="横竖屏提示"/>
+        <p className={styles.orientationText}>请保持{orientation === ORIENTATION_TEXT['竖屏'] ? '横屏' : '竖屏'}浏览</p>
+      </div>
     )
   }
 
@@ -52,18 +83,14 @@ export default class Orientation extends React.Component {
     const {orientation} = this.props
 
     if (currentOrientation === orientation) {
-      return (
-        <div className={cx(styles.orientationMask, styles.showMask)}>
-          {this.renderContent()}
-        </div>
-      )
+      return <div className={cx(styles.orientationMask, styles.showMask)}>{this.renderContent()}</div>
     }
     return null
   }
 }
 
 Orientation.defaultProps = {
-  orientation: 'landscape',
+  orientation: ORIENTATION_TEXT['横屏'],
   promptImg: 'https://s2.ax1x.com/2019/01/10/FOWfu8.png'
 }
 
